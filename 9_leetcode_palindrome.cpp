f01c73150d0f19290ff1866f57cd170fce623c7c
Roben <robenthapaaa@gmail.com>
2026-01-05 19:11:27 +0545

    palindrome

--- 9_leetcode_palindrome.cpp
+++ 9_leetcode_palindrome.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
 using namespace std;
 
+bool palindrome(int nums){
+    int remainder, x=nums;
+    long long ans;
+
+    while(nums>0){
+        remainder = nums%10;
+        ans = ans*10 + remainder;
+
+        nums /= 10;
+    }
+    if(x == ans){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 int main(){
+    int num = 601;
+    cout<<palindrome(num);
+
 
     return 0;
 }
