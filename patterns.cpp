10791abf0dacae4cb79f2608f554aef72460e3f9
Roben <robenthapaaa@gmail.com>
2026-01-07 21:19:20 +0545

    butterfly

--- patterns.cpp
+++ patterns.cpp
@@ -251,6 +251,47 @@ int hollow_diamond_patterns()
     return 0;
 }
 
+void butterfly(){
+    int n = 7;
+    for(int i=0; i<n; i++){
+        cout<<"\t\t";
+        for(int j=0; j<=i; j++){
+            cout<<"*";
+        }
+        for(int k = n-1; k>i; k--){
+            cout<<" ";
+        }
+        for(int l=n-1; l>i; l--){
+            cout<<" ";
+        }
+        for(int m=0; m<=i; m++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    
+    for(int i=0; i<n; i++){
+        cout<<"\t\t";
+        for(int j=n; j>i; j--){
+            cout<<"*";
+        }
+        for(int k=0; k<i; k++){
+            cout<<" ";
+        }
+        for(int l=0; l<i; l++){
+            cout<<" ";
+        }
+        for(int m=n; m>i; m--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return;
+}
+
+
 int main()
 {
     square_patterns();
@@ -266,6 +307,8 @@ int main()
     pyramid_patterns();
     cout << endl;
     hollow_diamond_patterns();
+    cout<<endl;
+    butterfly();
 
     return 0;
 }
