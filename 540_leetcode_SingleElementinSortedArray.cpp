fca0c9215be8d000b1019999dd579c2eb4653e22
Roben <robenthapaaa@gmail.com>
2026-01-14 18:35:24 +0545

    Single element in sorted array

--- 540_leetcode_SingleElementinSortedArray.cpp
+++ 540_leetcode_SingleElementinSortedArray.cpp
@@ -2,7 +2,42 @@
 #include<vector>
 using namespace std;
 
-int main(){
+int singleNonDuplicate(vector<int>& nums) {
+    int n = nums.size();
+    int st = 0, end = n - 1;
 
+    if(n==1) return nums[0];
+
+    while(st<=end){
+        int mid = st + (end -st)/2;
+
+        if(mid==0 && nums[0] != nums[1]) return nums[0];
+        if(mid == n-1 && nums[n-1] != nums[n-2]) return nums[mid];
+        if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]) return nums[mid];
+
+        if(mid%2==0){
+            if(nums[mid-1] == nums[mid]){
+                end = mid - 1;
+            }
+            else{
+                st = mid + 1;
+            }
+        }
+        else{
+            if(nums[mid-1] == nums[mid]){
+                st = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> nums = {3,3,7,7,10,11,11};
+
+    cout<<singleNonDuplicate(nums)<<endl;
     return 0;
 }
