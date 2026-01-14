71d1a069d0489a36b21ae85463e0628b25b92969
Roben <robenthapaaa@gmail.com>
2026-01-14 12:12:54 +0545

    Binary Search in rotated sorted array

--- BinearySearch_RotatedSorted.cpp
+++ BinearySearch_RotatedSorted.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int main(){
+vector<int> BinarySearch(vector<int> &nums, int target){
 
+}
+
+int main(){
+    vector<int> nums = {4,5,6,7,0,1,2};
+    int target = 0;
 
     return 0;
 }
