41c2ceaca65fa8d0be8033dde971e986f3882a19
Roben <robenthapaaa@gmail.com>
2026-01-15 15:18:13 +0545

    insertion sort

--- SortingAlgoritms.cpp
+++ SortingAlgoritms.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void bubbleSort(int arr[5]){
+void bubbleSort(int arr[5]){  // O(n^2)
     for(int i=0; i<4; i++){
         for(int j=0; j<5-i-1; j++){
             if(arr[j]>arr[j+1]){
@@ -15,10 +15,49 @@ void bubbleSort(int arr[5]){
     return;
 }
 
+void selectionSort(int arr[5]){  // O(n^2)
+    for(int i=0; i<5; i++){
+        int smallestidx = i;
+        for(int j=i+1; j<5; j++){
+            if(arr[j] <= arr[smallestidx]){
+                smallestidx = j;
+            }
+        }
+        swap(arr[i], arr[smallestidx]);
+    }
+
+    for(int i=0; i<5; i++){
+    cout<<arr[i];
+    }
+
+    return;
+}
+
+void insertionSort(int arr[5]){
+    for(int i=1; i<5; i++){
+        int curr = arr[i];
+        int prev = i-1;
+        
+        while(prev >= 0 && arr[prev] > curr){
+            arr[prev+1] = arr[prev];
+            prev--;
+        }
+        arr[prev+1] = curr;
+    }
+
+    for(int i=0; i<5; i++){
+    cout<<arr[i];
+    }
+
+    return;
+}
+
 int main(){
     int arr[] = {4, 1, 5, 2, 3};
 
     bubbleSort(arr);
+    selectionSort(arr);
+    insertionSort(arr);
 
     return 0;
 }
