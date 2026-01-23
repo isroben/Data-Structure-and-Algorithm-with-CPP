ac8e42764d279ddf87dfdffb09e2ba507bebb276
Roben <robenthapaaa@gmail.com>
2026-01-23 10:56:16 +0545

    remove all occurrances in a substring

---
+++ 1910_leetcode_removeAllOccurance.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string removeOccurrences(string s, string part) {
+    while(s.length() > 0 && s.find(part) < s.length()){
+        s.erase(s.find(part), part.length());
+    }
+    return s;
+}
+
+int main(){
+    string str = "daabcbaabcbc";
+    string part = "abc";
+
+    cout<<removeOccurrences(str, part);
+
+    return 0;
+}
