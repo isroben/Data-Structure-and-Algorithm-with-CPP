4d71d35c6d5eddace01b81e6facd73e243ea97fc
Roben <robenthapaaa@gmail.com>
2026-02-04 16:14:18 +0545

    sum of primary and secondary diagonal of matrix

--- sumOfDiagonal.cpp
+++ sumOfDiagonal.cpp
@@ -4,15 +4,26 @@ using namespace std;
 
 int diagonalSum(int nums[][4], int rows, int cols){
     int sum = 0;
-    for(int i=0; i<rows; i++){
-        for(int j=0; j<cols; j++){
-            if(i == j){
-                sum += nums[i][i];
-            }
-            else if(j == cols-i-1){
-                sum += nums[i][j];
-            }
+    // O(n*n)
+    // for(int i=0; i<rows; i++){
+    //     for(int j=0; j<cols; j++){
+    //         if(i == j){
+    //             sum += nums[i][i];
+    //         }
+    //         else if(j == cols-i-1){
+    //             sum += nums[i][j];
+    //         }
 
+    //     }
+    // }
+
+
+    // O(n)
+    for(int i=0; i<rows; i++){
+        sum += nums[i][i];
+        
+        if(i != rows-i-1){
+            sum += nums[i][rows-i-1];
         }
     }
 
