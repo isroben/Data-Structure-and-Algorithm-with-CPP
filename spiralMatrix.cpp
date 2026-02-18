7a90141c998fddd29a01d642ef3fa57859cb8c81
Roben <robenthapaaa@gmail.com>
2026-02-18 17:30:00 +0545

    fixed bug in spiral code

--- spiralMatrix.cpp
+++ spiralMatrix.cpp
@@ -9,18 +9,22 @@ vector<int> SpiralMatrix(vector<vector<int>> &nums){
     vector<int> ans;
 
     while(srow <= erow && scol <= ecol){
+        // Top
         for(int j=scol; j<= ecol; j++){
             ans.push_back(nums[srow][j]);
         }
-        for(int i=srow; i<=erow; i++){
+        // right
+        for(int i=srow+1; i<=erow; i++){
             ans.push_back(nums[i][ecol]);
         }
-        for(int j=ecol-1; j<=scol; j++){
+        // bottom
+        for(int j=ecol-1; j>=scol; j--){
             if(srow == erow) break;
             ans.push_back(nums[erow][j]);
         }
-        for(int i=erow-1; i<= srow+1; i++){
-            if(scol = ecol) break;
+        // left
+        for(int i=erow-1; i>=srow+1; i--){
+            if(scol == ecol) break;
             ans.push_back(nums[i][scol]);
         }
         srow++;
@@ -35,7 +39,7 @@ int main(){
     vector<vector<int>> nums = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}};
     vector<int> ans = SpiralMatrix(nums);
 
-    for(int i=0; i<=ans.size()+1; i++){
+    for(int i=0; i<=ans.size(); i++){
         cout<<i<<" ";
     }
 
