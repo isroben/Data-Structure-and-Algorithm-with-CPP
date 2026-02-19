f1ad6bea0c4f54b1677bc4116df099957a651472
Roben <robenthapaaa@gmail.com>
2026-02-19 14:57:06 +0545

    bruteforce apporach

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -1,3 +1,33 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
+vector<vector<int>> threeSome(vector<int> &nums){
+    int n = nums.size();
+    vector<vector<int>> ans;
+
+    set<vector<int>> s;
+
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            for(int k=j+1; k<n; k++){
+                if(nums[i] + nums[j] + nums[k] == 0){
+                    vector<int> trip = {nums[i] , nums[j] , nums[k]};
+                    sort(trip.begin(), trip.end());
+
+                    if(s.find(trip) != s.end()){
+                        s.insert(trip);
+                        ans.push_back(trip);
+                    }
+                }
+            }
+        }
+    }
+    return ans;
+}
+
+int main(){
+    
+
+    return 0;
+}
